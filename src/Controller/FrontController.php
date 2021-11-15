318cc8389383ddb1b2c8e06d608863dab6da0057
Myckadev <schneider.myckael@gmail.com>
2021-11-15 18:33:52 +0100

    add contact mailing and finish fixing lang issue

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -2,19 +2,41 @@
 
 namespace App\Controller;
 
+use App\Form\ContactFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FrontController extends AbstractController
 {
     /**
      * @Route("/contact", name="AppContact")
+     * @throws TransportExceptionInterface
      */
-    public function contact(){
+    public function contact(Request $request, MailerInterface $mailer){
+
+        $form = $this->createForm(ContactFormType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $email = (new Email())
+                ->from($form->get('email')->getData())
+                ->to(new Address('contact.bloutime@gmail.com', "KnowBook"))
+                ->subject($form->get('subject')->getData())
+                ->text($form->get('content')->getData());
+            $mailer->send($email);
+
+            return $this->redirectToRoute("AppHome");
+
+        }
 
         return $this->render('front/contact.html.twig', [
-
+            'contactForm'=>$form->createView()
         ]);
     }
 }
---
+++ src/Form/ContactFormType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', TextType::class, [
+                'label'=>false
+            ])
+            ->add('subject', TextType::class, [
+                'label'=>false
+            ])
+            ->add('content', TextareaType::class, [
+                'label'=>false,
+                'attr' => array('rows' => '10','cols' => '15')
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
