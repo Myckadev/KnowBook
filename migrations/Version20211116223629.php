702b100d233024a9ab9b326c86a8f79495dc37f0
Myckadev <schneider.myckael@gmail.com>
2021-11-17 02:38:21 +0100

    Add unit test

---
+++ migrations/Version20211116223629.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211116223629 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE blog_web (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, title VARCHAR(255) NOT NULL, content LONGTEXT NOT NULL, date DATETIME NOT NULL, description VARCHAR(100) NOT NULL, is_modify TINYINT(1) NOT NULL, modify_date DATETIME DEFAULT NULL, INDEX IDX_10ACB65BA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE reset_password_request (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, selector VARCHAR(20) NOT NULL, hashed_token VARCHAR(100) NOT NULL, requested_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', expires_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_7CE748AA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE review (id INT AUTO_INCREMENT NOT NULL, author_id INT DEFAULT NULL, blog_web_id INT DEFAULT NULL, content LONGTEXT NOT NULL, date DATETIME NOT NULL, INDEX IDX_794381C6F675F31B (author_id), INDEX IDX_794381C66B57E28C (blog_web_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, roles JSON NOT NULL, is_verified TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE blog_web ADD CONSTRAINT FK_10ACB65BA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE reset_password_request ADD CONSTRAINT FK_7CE748AA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE review ADD CONSTRAINT FK_794381C6F675F31B FOREIGN KEY (author_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE review ADD CONSTRAINT FK_794381C66B57E28C FOREIGN KEY (blog_web_id) REFERENCES blog_web (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE review DROP FOREIGN KEY FK_794381C66B57E28C');
+        $this->addSql('ALTER TABLE blog_web DROP FOREIGN KEY FK_10ACB65BA76ED395');
+        $this->addSql('ALTER TABLE reset_password_request DROP FOREIGN KEY FK_7CE748AA76ED395');
+        $this->addSql('ALTER TABLE review DROP FOREIGN KEY FK_794381C6F675F31B');
+        $this->addSql('DROP TABLE blog_web');
+        $this->addSql('DROP TABLE reset_password_request');
+        $this->addSql('DROP TABLE review');
+        $this->addSql('DROP TABLE user');
+    }
+}
---
+++ tests/UserTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Tests;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+/**@test*/
+class UserTest extends WebTestCase
+{
+    public function testUserCanDestroyBlog(): void
+    {
+
+        $user = (new User())
+            ->setUsername("Jean")
+            ->setEmail(10)
+            ->setIsVerified(0)
+            ->setPassword("Test")
+            ->setRoles(array('ROLE_MEMBER'));
+
+        $kernel = self::bootKernel();
+        $error = self::$container->get('validator')->validate($user);
+        $this->assertCount(0, $error);
+        //$this->assertSame('test', $kernel->getEnvironment());
+        //$routerService = self::$container->get('router');
+        //$myCustomService = self::$container->get(CustomService::class);
+    }
+
+//    public function testUserCantLoadPage(){
+//        $client = static::createClient();
+//        $client->request('GET', '/admin');
+//        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+//
+//    }
+
+    public function testUserCanLoadPage(){
+        $client = static::createClient();
+        $client->request('GET', '/');
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+
+    }
+
+    public function testLoginWithBadCredentials(){
+        $client = static::createClient();  //Générer un client
+        $crawler = $client->request('GET', '/login'); //Envoyé le client sur la route login
+        $form = $crawler->selectButton('Connexion')->form([  //tester de remplir le formulaire avec des données
+            'email' => "test@test.fr",
+            'password' => "fakepassword"
+        ]);
+        $client->submit($form); //Envoyé le formulaire
+        $this->assertResponseRedirects('/login'); //On sait que le mot de passe est faux alors on s'attend à une redirection sur la page login
+        $client->followRedirect(); //On dit au client de suivre la redirection
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK); //si tout est bon jusque la alors la page login c'est bien rechargé et le status code est 200
+    }
+}
